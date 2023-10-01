b0a54c1be6328f1f77f0d2435a835c51d172b253
Jiří Štípek <91186480+Jurgee@users.noreply.github.com>
2023-10-01 20:02:32 +0200

    innit

---
+++ 5.cviko/ITU-Lab-Starter/WPFWeather.App/Windows/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using WPFWeather.App.Services;
+using WPFWeather.App.ViewModels;
+
+namespace WPFWeather.App.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WeatherViewModel viewModel = new WeatherViewModel(new WeatherDiskService());
+            DataContext = viewModel;
+        }
+    }
+}
